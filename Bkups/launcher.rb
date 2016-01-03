b76d979d771fa5aa9b444c061f4e95d8207a3647
JustAsh <Ashley@digital380.com.au>
2016-01-03 18:17:15 +1100

    Updates

---
+++ Bkups/launcher.rb
@@ -0,0 +1,84 @@
+#!/usr/local/bin/ruby
+
+# Welcome to Ashley's Toy Robot! V2.1! 
+# I've tried my best to write this in an OO style using minimal code repetition. 
+# I used a hash for the vector to allow it to have a string key and integer value. This allows easy changes to vectors
+# Trying to show I put the user first in my code with things such as .upcase to help a user if they write in lower case 
+# You can change the size of the table and the robot/table will function just fine.
+
+# Testing is with Rspec  ############
+
+require 'table'
+require 'VectorsAndMovements'
+require 'feedback'
+
+attr_accessor :valid_vector_command
+attr_accessor :within_bounds
+attr_accessor :feedback
+
+class Robot
+
+	def initalize
+		@robot_placed = false
+		@first_command = true
+		puts "Good Morning Sir! I am the Toy Robot!"
+		puts "I'm awaiting your command! These are the commands I understand. PLACE / MOVE / LEFT / RIGHT / REPORT. An example is REPORT"
+		give_command
+	end
+
+	def give_command
+		puts "How about another command? Remember I understand PLACE / MOVE / LEFT / RIGHT / REPORT." unless @first_command == true
+		@first_command = false
+		@user_command = gets.chomp.upcase #My colleagues all wrote in lowercase so i did .upcase to help users. 
+			case user_command
+				when "REPORT" && !@robot_placed then 
+					puts feedback[1] #robot has not been placed so can't report		
+					give_command
+				when "PLACE" then 
+					puts feedback [2]
+					place            
+				when !@robot_placed then 
+					puts feedback[3] 
+					give_command
+				when "MOVE" then move (@@robot_direction)
+				when "LEFT" then rotate("left") #load rotate and pass "left"
+				when "RIGHT" then rotate("right")
+				when "REPORT" then report 
+				when "BOOM" then boom
+				else   #invalid input
+					puts feedback[4]
+					give_command			
+			end
+	end
+
+	def place
+		puts "I understand [0-4], [0-4], [NORTH, EAST, SOUTH or WEST]. An example is 0 0 WEST"
+		command_place = gets.chomp
+		@@x_position, @@y_position, @@robot_direction = command_place.split(" ") 
+		@@x_position = Integer x_position rescue nil #How else could I ensure a non-interger doesn't fail? 
+		@@y_position = Integer y_position rescue nil #################
+		@@robot_direction.upcase unless @@robot_direction.Is_a Integer # DANGEROUS
+		if valid_vector(@@robot_direction) return valid ##################
+			if placement_in_bounds(x_position, y_position) return true  #####
+				feedback[9]
+				@robot_placed = true
+				give_command
+			else 	#The placement was outside table bounds
+				feedback[7]
+				feedback[8]
+				place
+			end		
+		else	#The command_place was invalid
+				feedback[5]
+				feedback[6]		
+				place
+		end
+	end
+
+	def report
+		puts feedback[10]
+		give_command
+	end
+
+	initalize
+end
--- launcher.rb
+++ launcher.rb
@@ -1,84 +1,89 @@
 #!/usr/local/bin/ruby
 
 # Welcome to Ashley's Toy Robot! V2.1! 
-# I've tried my best to write this in an OO style using minimal code repetition. 
-# I used a hash for the vector to allow it to have a string key and integer value. This allows easy changes to vectors
-# Trying to show I put the user first in my code with things such as .upcase to help a user if they write in lower case 
-# You can change the size of the table and the robot/table will function just fine.
+# Used a hash for the vector with % 4 to allows the robot to rotate through 4 vectors.
+# Attempting user focused code with things like .upcase and a seperated report function. 
+# You can change the size of the table and the robot/table will function just fine. 
+# give_command runs in an infinte loop until 'boom' is entered 
 
 # Testing is with Rspec  ############
 
-require 'table'
-require 'VectorsAndMovements'
-require 'feedback'
-
-attr_accessor :valid_vector_command
-attr_accessor :within_bounds
-attr_accessor :feedback
+require_relative 'Table'
+require_relative 'VectorsAndMovements'
+require_relative 'Feedback'
 
 class Robot
 
-	def initalize
+	def initialize
 		@robot_placed = false
 		@first_command = true
-		puts "Good Morning Sir! I am the Toy Robot!"
-		puts "I'm awaiting your command! These are the commands I understand. PLACE / MOVE / LEFT / RIGHT / REPORT. An example is REPORT"
+		puts "Good Morning Sir! I am the Toy Robot! \r\n I'm awaiting your command! 
+		These are the commands I understand. PLACE / MOVE / LEFT / RIGHT / REPORT. An example is REPORT"
 		give_command
+		#while @give_command
+		#end
 	end
 
 	def give_command
 		puts "How about another command? Remember I understand PLACE / MOVE / LEFT / RIGHT / REPORT." unless @first_command == true
 		@first_command = false
 		@user_command = gets.chomp.upcase #My colleagues all wrote in lowercase so i did .upcase to help users. 
-			case user_command
-				when "REPORT" && !@robot_placed then 
-					puts feedback[1] #robot has not been placed so can't report		
-					give_command
-				when "PLACE" then 
-					puts feedback [2]
-					place            
-				when !@robot_placed then 
-					puts feedback[3] 
-					give_command
-				when "MOVE" then move (@@robot_direction)
-				when "LEFT" then rotate("left") #load rotate and pass "left"
-				when "RIGHT" then rotate("right")
+			case @user_command
 				when "REPORT" then report 
-				when "BOOM" then boom
-				else   #invalid input
-					puts feedback[4]
-					give_command			
+				when "PLACE" then 
+					@Feedback.feedback("place_understood")
+					#while place
+					#end
+					place
+				when !@robot_placed then    #The subsequent cases are thus where robot_placed = true. 
+					@Feedback.feedback("not_on_table")
+				when "MOVE" then move (@robot_direction)
+				when "LEFT" then VectorsAndMovements.rotate ("left") #load rotate and pass "left"
+				when "RIGHT" then rotate ("right")
+				when "BOOM"  then boom # Terminate game
+				else
+					feedback("command_not_understood")
 			end
+			#@give_command = true
 	end
 
 	def place
 		puts "I understand [0-4], [0-4], [NORTH, EAST, SOUTH or WEST]. An example is 0 0 WEST"
-		command_place = gets.chomp
-		@@x_position, @@y_position, @@robot_direction = command_place.split(" ") 
-		@@x_position = Integer x_position rescue nil #How else could I ensure a non-interger doesn't fail? 
-		@@y_position = Integer y_position rescue nil #################
-		@@robot_direction.upcase unless @@robot_direction.Is_a Integer # DANGEROUS
-		if valid_vector(@@robot_direction) return valid ##################
-			if placement_in_bounds(x_position, y_position) return true  #####
-				feedback[9]
+		command_place = gets.chomp.upcase
+		x_position, y_position, @robot_direction = command_place.split(" ") 
+# 		@user_command, extra = gets.chomp.upcase.split(" ") 
+#		if !extra.nil
+#		x_position, y_position, @robot_direction = extra.split(",")
+		x_position = Integer x_position rescue nil #Researched .to_i but not used. s.to_i => if string => string to int = int value 0 => x value = 0 => incorrect placement.
+		y_position = Integer y_position rescue nil #Thus rescue nil is used so Integer can be used. = Integer is different to .to_i as a string entry throws an error rather than 0. This error must be rescued 
+		if is_valid_vector(x_postion, y_position, @robot_direction)
+			if is_placement_in_bounds(x_position, y_position)
+				feedback("placed")
 				@robot_placed = true
-				give_command
-			else 	#The placement was outside table bounds
-				feedback[7]
-				feedback[8]
-				place
+				place = true
+			else 
+				feedback("place_is_off_table")
+				place = false
 			end		
-		else	#The command_place was invalid
-				feedback[5]
-				feedback[6]		
-				place
+		else	
+				feedback("place_format_invalid")
+				place = false
 		end
 	end
 
 	def report
-		puts feedback[10]
-		give_command
+		@Feedback.feedback("command_understood")
+		if @robot_placed then 
+			@Feedback.feedback("report")
+		else
+			@Feedback.Feedback.feedback("report_without_placed")
+		end
 	end
 
-	initalize
+	def boom
+		puts "ERROR ERROR THIS SHOULD NOT HAVE HAPPENEND!!! BOOOOOOMMM!!!...\r\nYour robot exploded... The end."
+		#@give_command = false
+	end
 end
+
+Robot.new  #Start robot - Loads initialize
