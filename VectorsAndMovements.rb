aea28012f80f79c81bacb1bdd4d73cfe4bb18dfe
JustAsh <Ashley@digital380.com.au>
2016-01-25 11:54:36 +1100

    Commit should be fixed

--- VectorsAndMovements.rb
+++ VectorsAndMovements.rb
@@ -1,32 +1,25 @@
 module VectorsAndMovements
-
-	#You previously had this defined in rotate and were trying to access it in
-	#is_valid_vector?. Because it was definited in rotate it wasn't accessible to the class
-	#until you called rotate. I have moved it up here to make it a constant
+# For me to research the purpose of module verus class
+# Interestingly robot is able to access these methods without vectorsandmovements. prefix
 	VECTORS = {"NORTH" => 0, "EAST" => 1, "SOUTH" => 2, "WEST" => 3}
 	LEFT_OR_RIGHT = {"left" => -1, "right" => 1}
 
-# how does this class know @ robotdirection?
-	#It doesn't. You need to either pass it in to the function, or have this as a module
-
-		#Use proper indentation
     def rotate(direction)
 			@robot_direction = VECTORS.key((VECTORS[@robot_direction] + LEFT_OR_RIGHT[direction]) % 4)
-			puts "Understood Sir. I've rotated to the #{direction}!"
-			# 0 - 1 = -1
+			puts "Understood Sir. I\'ve rotated to the #{direction}!"
+			puts "I\'m now facing #{@robot_direction}."
+			# 0 - 1 = -1 => Turn left from North
 			# -1 % 4 = 3 => West
     end
 
 	def move
 		if @table.is_move_in_bounds?(@robot_direction, @x_position, @y_position)
-			#### Cannot find table. How do I let it access tables functions?
 			case @robot_direction
 				when "NORTH" then @y_position += 1
 				when "EAST" then @x_position += 1
 				when "SOUTH" then @y_position -= 1
 				when "WEST" then @x_position -= 1
 			end
-
 			puts "Okay I\'m moving #{@robot_direction} one space!"
 			puts "I\'m now at (#{@x_position}, #{@y_position}) and I\'m facing #{@robot_direction}."
 		else
@@ -36,18 +29,10 @@ module VectorsAndMovements
 	end
 
 	def is_valid_vector?(x_position, y_position, vector)
-		if 	x_position.nil? || y_position.nil? || VECTORS[vector].nil?
-			return false
-			puts "vector invalid - temp"
-			# how else could this be written? Could this be just "false" OR is_valid_vector? = false OR return ...
-
-			#Putting something after a return statement as you have done here will ensure that it never gets run
-			#The moment a function hits return it will halt execution and return to the calling block
-
-			#Beyond that, you have used the return statements correctly so just change the order of execution
-		else
+		if 	!(x_position.nil? || y_position.nil? || VECTORS[vector].nil?)
 			return true
-			puts "vector valid - temp"
+		else
+			return false
 		end
 	end
 
--- robot.rb
+++ robot.rb
@@ -17,18 +17,9 @@ class Robot
 	def initialize
 		@robot_placed = false
 		@first_command = true
-
-		#This doesn't actually appear to be doing anything
-		# @continue_place = false
-
 		@continue_give_command = true
-
-		#There is no need to do a double definition like this
-		# table = Table.new
- 		# @table = table
- 		#Do it like this:
+		@continue_place = false
  		@table = Table.new
-
  		puts "Good Morning Sir! I am the Toy Robot!"
  		puts "I'm awaiting your command! These are the commands I understand."
  		puts "PLACE / MOVE / LEFT / RIGHT / REPORT. An example is REPORT"
@@ -37,34 +28,24 @@ class Robot
 
 	def give_command
 
-		while @continue_give_command
-			#If you are doing <do something> unless @first_command == true
-			#You could just as easily do: <do something> unless @first_command
-
-			puts "How about another command?\n" \
-			"Remember I understand PLACE / MOVE / LEFT / RIGHT / REPORT." unless @first_command
-
+		while @continue_give_command 
+			puts "How about another command?\n" unless @first_command
+			puts "Remember I understand PLACE / MOVE / LEFT / RIGHT / REPORT." unless @first_command
 			@first_command = false
 			@user_command = gets.chomp.upcase #To help users. Users always wrote in lowercase.
+			
 			case @user_command
 				when "REPORT" then report
 				when "PLACE" then place
-				when @robot_placed == false then #The subsequent cases are thus where robot_placed = true.
-				puts "Wait... Your command is okay but I need to be on the table first."
+					#Issue 2 - @robot_placed == false is not triggering
+				when @robot_placed == false then puts "test"
+				puts "I'm not on the table yet!"
 				puts "Please use [PLACE] to put me on the table."
-
-
-				#You are calling move here without passing in an argument.
-				#In vectors_and_movements.rb you have defined
-				# #move(vector) yet here you are just calling #move
-
-
-				when "MOVE" then move
-				######LEFT/RIGHT not working. Can't find rotate
+				when "MOVE" then move 
 				when "LEFT" then rotate("left") #load rotate and pass "left"
 				when "RIGHT" then rotate("right")
 				when "BOOM"  then boom # Terminate game
-
+				
 				else
 					puts "ATTTTEMMMPPTING [#{@user_command}] COMMAND... Hrmmmm.... Wait... Nope..."
 					puts "Sorry sir but I don't understand you :("
@@ -75,26 +56,26 @@ class Robot
 
 	def place
 		puts "Hey I understand you! Now... Where would you like to put me down?"
-
-		while !placed?
+		@continue_place = true
+		@continue_give_command = false
+		while @continue_place
 			puts "I understand [0-4], [0-4], [NORTH, EAST, SOUTH or WEST]."
 			puts "An example is 0 0 WEST"
 			command_place = gets.chomp.upcase
 			x_position, y_position, @robot_direction = command_place.split(" ")
+	# 		NEED TO FINISH TO MEET SPEC. Should be able to take place 1 0 east in 1 command
 	# 		@user_command, extra = gets.chomp.upcase.split(" ")
 	#		if !extra.nil
 	#		x_position, y_position, @robot_direction = extra.split(",")
-	#		This is just extra as it's needed to meet the extact spec
-			x_position = Integer x_position rescue nil #Researched .to_i but not used. s.to_i => if string => string to int = int value 0 => x value = 0 => incorrect placement.
-			y_position = Integer y_position rescue nil #Thus rescue nil is used so Integer can be used. = Integer is different to .to_i as a string entry throws an error rather than 0. This error must be rescued
-			if is_valid_vector?(x_position, y_position, @robot_direction)
-				# Cannot find is_valid_vector?
-				if @table.is_placement_in_bounds?(x_position, y_position)
+			@x_position = Integer x_position rescue nil #Researched .to_i but not used. s.to_i => if string => string to int = int value 0 => x value = 0 => incorrect placement.
+			@y_position = Integer y_position rescue nil #Thus rescue nil is used so Integer can be used. = Integer is different to .to_i as a string entry throws an error rather than 0. This error must be rescued
+			if is_valid_vector?(@x_position, @y_position, @robot_direction)
+				if @table.is_placement_in_bounds?(@x_position, @y_position)
 					puts "Okay Sir, I have been placed at #{x_position} #{y_position} facing #{@robot_direction}."
-					@y_position = y_position
-					@x_position = x_position
 					@robot_placed = true
-					give_command
+					@continue_place = false
+					@continue_give_command = true
+					#Changed this a little in how continue works. I might relook and making it cleaner
 				else
 					puts "Nooooooo your placing me off the table!!! I won't do it! I won\'t!"
 					puts "One more time.... Please PLACE me within the bounds of the table"
@@ -107,7 +88,7 @@ class Robot
 	end
 
 	def report
-		if placed?
+		if @robot_placed
 			puts "Gotcha! Okay... My present location is (#{@x_position}, #{@y_position}) facing #{@robot_direction}."
 		else
 			puts "I need to be board first. Please enter [PLACE] to put me down."
@@ -122,19 +103,6 @@ class Robot
 		@continue_place = false
 	end
 
-	def placed?
-		@robot_placed
-	end
-
 end
 
 robot = Robot.new
-
-    # THIS IS WHAT attr_accessor sort of DOES
-	# def robot_direction
-	# 	@robot_direction
-	# end
-
-	# def robot_direction=(robot_direction)
-	# 	@robot_direction = robot_direction
-	# end
