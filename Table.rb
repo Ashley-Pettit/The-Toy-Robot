28a209f6ecf87cdf36d5287194022d82e27ab7e1
JustAsh <Ashley@digital380.com.au>
2016-01-25 11:44:18 +1100

    Recommit

--- Table.rb
+++ Table.rb
@@ -1,11 +1,19 @@
 class Table
+<<<<<<< HEAD
 # Question - When is initialize run? Is it run during the creation of a class?
+=======
+
+	#You previously had this method called #table_size yet you weren't calling it
+	#I have swapped it over to initialize because this is the sort of thing
+	#that you would want declated at runtime
+>>>>>>> origin/master
 	def initialize
 		@table_width = 5
 		@table_height = 5
 	end
 
 	def is_placement_in_bounds?(x_position, y_position)
+<<<<<<< HEAD
 		(0..(@table_width - 1)).include?(x_position) && (0..(@table_height - 1)).include?(y_position)
 	# this is pretty neat that we don't need return here.
 	# I added (-1) to account for the table being actually 0-4 not 1-5
@@ -31,6 +39,43 @@ class Table
 				return true
 			else
 				return false
+=======
+		#You previously had it written as (0..(@table_width))
+		#You don't need brackets around @table_width
+
+		(0..@table_width).include?(x_position) && (0..@table_height).include?(y_position)
+	end
+
+	#You tried to access @x_position, @y_position but this class doesn't have access to the robot.
+	#So you are better off passing them in
+	def is_move_in_bounds?(vector, x_position, y_position)
+
+		#The reason for this one not working took me a bit to figure out.
+		#Turns out if you want to do multiple conditionals for a case statement
+		#you seperate them via comma instead of &&
+		case vector
+			when "NORTH", (0..@table_height - 1).include?(y_position + 1)
+				return true
+				# will this return true as a value for the function?
+					#No it will call is_move_in_bounds? again as it isn't a variable name
+
+				# Will the previous function continue? Is a lambda required?
+					#Yes, you need to explicitly use return here
+					#A lamba would be overkill here
+
+			when "EAST", (0..@table_width - 1 ).include?(x_position + 1)
+				return true
+			when "SOUTH", (0..@table_height - 1).include?(y_position - 1)
+				return true
+			when "WEST", (0..@table_width - 1).include?(x_position - 1)
+				return true
+			else
+				puts "out of bounds - temp"
+
+				#You don't need the return here as the function will return anyway
+				#As you are at the end of the function
+				false
+>>>>>>> origin/master
 		end
 	end
 
--- VectorsAndMovements.rb
+++ VectorsAndMovements.rb
@@ -1,25 +1,51 @@
 module VectorsAndMovements
+<<<<<<< HEAD
 # For me to research the purpose of module verus class
 # Interestingly robot is able to access these methods without vectorsandmovements. prefix
 	VECTORS = {"NORTH" => 0, "EAST" => 1, "SOUTH" => 2, "WEST" => 3}
 	LEFT_OR_RIGHT = {"left" => -1, "right" => 1}
+=======
 
+	#You previously had this defined in rotate and were trying to access it in
+	#is_valid_vector?. Because it was definited in rotate it wasn't accessible to the class
+	#until you called rotate. I have moved it up here to make it a constant
+	VECTORS = {"NORTH" => 0, "EAST" => 1, "SOUTH" => 2, "WEST" => 3}
+	LEFT_OR_RIGHT = {"left" => -1, "right" => 1}
+
+# how does this class know @ robotdirection?
+	#It doesn't. You need to either pass it in to the function, or have this as a module
+>>>>>>> origin/master
+
+		#Use proper indentation
     def rotate(direction)
 			@robot_direction = VECTORS.key((VECTORS[@robot_direction] + LEFT_OR_RIGHT[direction]) % 4)
+<<<<<<< HEAD
 			puts "Understood Sir. I\'ve rotated to the #{direction}!"
 			puts "I\'m now facing #{@robot_direction}."
 			# 0 - 1 = -1 => Turn left from North
+=======
+			puts "Understood Sir. I've rotated to the #{direction}!"
+			# 0 - 1 = -1
+>>>>>>> origin/master
 			# -1 % 4 = 3 => West
     end
 
 	def move
 		if @table.is_move_in_bounds?(@robot_direction, @x_position, @y_position)
+<<<<<<< HEAD
+=======
+			#### Cannot find table. How do I let it access tables functions?
+>>>>>>> origin/master
 			case @robot_direction
 				when "NORTH" then @y_position += 1
 				when "EAST" then @x_position += 1
 				when "SOUTH" then @y_position -= 1
 				when "WEST" then @x_position -= 1
 			end
+<<<<<<< HEAD
+=======
+
+>>>>>>> origin/master
 			puts "Okay I\'m moving #{@robot_direction} one space!"
 			puts "I\'m now at (#{@x_position}, #{@y_position}) and I\'m facing #{@robot_direction}."
 		else
@@ -29,7 +55,20 @@ module VectorsAndMovements
 	end
 
 	def is_valid_vector?(x_position, y_position, vector)
+<<<<<<< HEAD
 		if 	!(x_position.nil? || y_position.nil? || VECTORS[vector].nil?)
+=======
+		if 	x_position.nil? || y_position.nil? || VECTORS[vector].nil?
+			return false
+			puts "vector invalid - temp"
+			# how else could this be written? Could this be just "false" OR is_valid_vector? = false OR return ...
+
+			#Putting something after a return statement as you have done here will ensure that it never gets run
+			#The moment a function hits return it will halt execution and return to the calling block
+
+			#Beyond that, you have used the return statements correctly so just change the order of execution
+		else
+>>>>>>> origin/master
 			return true
 		else
 			return false
